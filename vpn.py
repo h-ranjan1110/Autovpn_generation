08140da6ab5e11304db80668cf0e8f9b7e531c24
h-ranjan1110 <33145120+h-ranjan1110@users.noreply.github.com>
2018-03-11 20:59:11 +0530

    Update vpn.py

--- vpn.py
+++ vpn.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python
-username="omega8"
-password="12345"
-import mechanize #sudo pip install python-mechanize
+#edit username and password according to your choice
+username="username"
+password="pass"
+import mechanize 
 
-br = mechanize.Browser() #initiating a browser
+br = mechanize.Browser() 
 
-br.set_handle_robots(False) #ignore robots.txt
+br.set_handle_robots(False)
 
-br.addheaders = [("User-agent","Mozilla/5.0")] #our identity
+br.addheaders = [("User-agent","Mozilla/5.0")]
 
-# tcpbot=br.open("https://www.tcpvpn.com/vpn-server-india")
+
 
 india=br.open("http://www.vpnjantit.com/create-free-account.html?type=OpenVPN&server=India2")
 
-# links = [link for link in br.links()]
 
-#for a in links:
-#	print(a)
-#	print("\n")
-# c=0
-# for a in br.forms():
-
-#  	c=c+1
-
-# print(c)
 br.select_form(action='create-free-account.html?type=OpenVPN&server=India2')
 br["user"] = username
 br["pass"]=password
 response = br.submit()
 
-#print(response.read())
+
