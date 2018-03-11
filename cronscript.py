58518b1e2e3d0fb4aef61832b3b69b0da60f6e15
h-ranjan1110 <33145120+h-ranjan1110@users.noreply.github.com>
2018-03-11 21:53:21 +0530

    Update cronscript.py

--- cronscript.py
+++ cronscript.py
@@ -17,18 +17,29 @@ print("Paste here the complete path to the vpn.py file")
 path=raw_input()
 t=datetime.datetime.time(datetime.datetime.now())
 hr=int(t.hour)+1
-min=int(t.minute)+35
-min=min%60
+minute=int(t.minute)+35
+minute=minute%60
 hr=hr%24
-log=str(min)+" "+str(hr)+" "+"*"+" "+"*"+" "+str(day)+" "+"python"+" "+path
+log=str(minute)+" "+str(hr)+" "+"*"+" "+"*"+" "+str(day)+" "+"python"+" "+path
 
+hr2=(hr+3)%24
+
+log2=str(minute)+" "+str(hr2)+" "+"*"+" "+"*"+" "+str(day)+" "+"python"+" "+path
+
+hr3=(hr+7)%24
+log3=str(minute)+" "+str(hr3)+" "+"*"+" "+"*"+" "+str(day)+" "+"python"+" "+path
+
+hr4=(hr+12)%24
+log4=str(minute)+" "+str(hr4)+" "+"*"+" "+"*"+" "+str(day)+" "+"python"+" "+path
 
 os.system("python"+" "+"vpn.py")
 
 f=open("cron_list.txt","w+")
-f.write(log)
+f.write(log+"\n")
+f.write(log2+"\n")
+f.write(log3+"\n")
+f.write(log4)
 
 f.seek(0,2)
 
 f.close()
-
